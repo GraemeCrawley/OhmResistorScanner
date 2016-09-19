3f6a2545828797cd7bcc46c871004368edab0bfd
Ryan Marks <marksr2@mcmaster.ca>
2016-09-19 15:37:44 -0400

    Added initial project files

---
+++ src/src/test/java/ExampleTest.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by Ryan on 19/09/2016.
+ * This class demonstrates simple test writing
+ */
+public class ExampleTest {
+
+    @Test
+    public void testAddition(){
+        assertEquals("1 + 1 = 2", 2, 1+1);
+    }
+
+
+}
