34cebb5109f20cfbcbf75a4c0894655c92b1346d
Jonathan Brown <brownjs2@mcmaster.ca>
2016-09-20 15:56:42 -0400

    "Added a Resources folder, starting to work on a basic UI to work with."

--- src/src/main/java/Main.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Ryan on 19/09/2016.
- * This is a main class to verify that our build system works
- */
-public class Main {
-    public static void main(String... args){
-        System.out.println("Hello, world");
-    }
-}
---
+++ src/src/main/ohm/ohmUI.java
@@ -0,0 +1,18 @@
+package ohm; /**
+ * Created by jon on 2016-09-20.
+ */
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class ohmUI extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
