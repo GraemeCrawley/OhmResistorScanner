d4b7a746fc7788490d917d9b7f267ded4df14e30
Jonathan Brown <brownjs2@mcmaster.ca>
2016-09-20 19:01:25 -0400

    Implemented the very basics of the UI

---
+++ src/src/main/ohm/Ohm.java
@@ -0,0 +1,39 @@
+package ohm;/**
+ * Created by jon on 2016-09-20.
+ */
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import org.opencv.core.Core;
+
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+
+public class Ohm extends Application {
+
+    public static void main(String[] args) throws Exception{
+        System.out.println("Hello, world");
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+
+        Writer writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("filename.txt"), "utf-8"));
+        writer.write("something");
+
+
+
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        System.out.println(getClass().toString());
+        Parent root = FXMLLoader.load(getClass().getResource("OhmView.fxml"));
+        primaryStage.setTitle("Ohm");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+}
---
+++ src/src/main/ohm/OhmViewController.java
@@ -0,0 +1,20 @@
+package ohm; /**
+ * Created by jon on 2016-09-20.
+ */
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import org.opencv.core.Core;
+
+public class OhmViewController{
+
+    @FXML
+    public void buttonClicked(ActionEvent actionEvent) {
+        System.out.println("Button was clicked");
+    }
+}
